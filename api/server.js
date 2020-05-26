ed4506b5cd72e80851d669d1d88137eec5232c43
Wesley Ruedebusch <w.ruedebusch@gmail.com>
2020-05-26 17:01:03 -0400

    server up and running after forgetting export

--- api/server.js
+++ api/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+require('dotenv').config()
 
 const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
 
 server.get('/', (req, res) => {
     res.status(200).json({ message: 'Server up and running' })
 })
+
+module.exports = server;
