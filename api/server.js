f5add4d2faf7cd357980c514f2ec3e17ed1094ee
Wesley Ruedebusch <w.ruedebusch@gmail.com>
2020-05-26 23:16:35 -0400

    class and asccounts files made

--- api/server.js
+++ api/server.js
@@ -3,11 +3,15 @@ const cors = require('cors');
 const helmet = require('helmet');
 require('dotenv').config()
 
+const authRouter = require('../auth/authRouter');
+const authenticator = require('../auth/authenticator');
+
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use('/auth',authRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json({ message: 'Server up and running' })
